785dfd88bfb6517a9f7bf9ff407557d6433fc6bf
Rodrigues, Luiz C <luiz.c.rodrigues@accenture.com>
2020-07-19 12:42:14 -0300

    SuperTrend

--- tradeBotSystem.py
+++ tradeBotSystem.py
@@ -45,10 +45,10 @@ class Terminal(cmd.Cmd):
         return True
     
     def do_config(self, line):
-        self.tickers = str(input('Tickers [VALE3.SA PETR4.SA]: ') or 'VALE3.SA')
-        self.period =  str(input('Period [1d, 15m]: ') or '60m')
-        self.startDate = str(input('Start Date [YYYY-MM-DD]: ') or '2020-07-10')
-        self.endDate = str(input('End Date [YYYY-MM-DD]: ') or '2020-07-17')
+        self.tickers = str(input('Tickers [VALE3.SA]: ') or 'VALE3.SA')
+        self.period =  str(input('Period [60m]: ') or '60m')
+        self.startDate = str(input('Start Date [2020-07-10]: ') or '2020-07-10')
+        self.endDate = str(input('End Date [202-07-17]: ') or '2020-07-17')
         self.stPeriod = int(input('SuperTrend Period [7]: ') or 7)
         self.stFactor = int(input('SuperTrend Factor [3]: ') or 3)
 
